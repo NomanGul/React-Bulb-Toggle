9c8864febd28481f5d379c4651bab020007a22fd
Noman Gul <nomangul2001@gmail.com>
2018-09-05 14:59:57 +0500

    Styling and Comments Added

--- src/components/FloatingButton/FloatingButton.js
+++ src/components/FloatingButton/FloatingButton.js
@@ -3,22 +3,35 @@ import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import BrokenImage from "@material-ui/icons/BrokenImage";
 
+// STYLES
 const styles = theme => ({
   button: {
-    margin: theme.spacing.unit
+    margin: theme.spacing.unit,
+    backgroundColor: "red"
   },
   extendedIcon: {
     marginRight: theme.spacing.unit
+  },
+  icon: {
+    color: "white"
   }
 });
 
-function FloatingButton(props) {
+// FLOATING BUTTON FUNCTIONAL COMPONENT FOR BROKEN BULB
+const FloatingButton = props => {
   const { classes } = props;
-  return <div className="floating_btn">
-    <Button variant="fab" color="primary" aria-label="Add" className={classes.button} onClick={props.FloatingButtonClicked}>
-        <BrokenImage />
+  return (
+    <div className="floating_btn">
+      <Button
+        variant="fab"
+        aria-label="Add"
+        className={classes.button}
+        onClick={props.FloatingButtonClicked}
+      >
+        <BrokenImage className={classes.icon} />
       </Button>
-    </div>;
-}
+    </div>
+  );
+};
 
 export default withStyles(styles)(FloatingButton);
